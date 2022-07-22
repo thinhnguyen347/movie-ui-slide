a679c3198db310377f844748bf13f41e43b2802c
unknown <thinh.ohg@gmail.com>
2022-07-22 18:50:36 +0700

    update

---
+++ lib/components/movie_detail.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MovieDetail extends StatelessWidget {
+  const MovieDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          iconTheme: const IconThemeData(color: Colors.black),
+          actions: const [
+            Padding(
+                padding: EdgeInsets.only(right: 16.0),
+                child: Icon(Icons.favorite_border))
+          ],
+        ));
+  }
+}
