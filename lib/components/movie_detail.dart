ed73f5a6cf3d6df957a17a6d9d8c1636b2b77152
thinh.ohg@gmail.com <ziczac//91>
2022-07-29 00:33:32 +0700

    update

--- lib/components/movie_detail.dart
+++ lib/components/movie_detail.dart
@@ -16,6 +16,63 @@ class MovieDetail extends StatelessWidget {
                 padding: EdgeInsets.only(right: 16.0),
                 child: Icon(Icons.favorite_border))
           ],
+        ),
+        body: Stack(
+          children: [
+            Image.asset("assets/images/bg.jpg",
+                fit: BoxFit.cover, height: MediaQuery.of(context).size.height),
+            Container(
+                height: MediaQuery.of(context).size.height,
+                color: Colors.white.withOpacity(0.8)),
+            _body(context),
+          ],
         ));
   }
+}
+
+_body(BuildContext context) {
+  return ListView(
+    shrinkWrap: true,
+    physics: const ClampingScrollPhysics(),
+    children: [const SizedBox(height: 20), _header()],
+  );
+}
+
+Widget _header() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+    child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          clipBehavior: Clip.antiAlias,
+          child: Image.asset("assets/images/conan.jpg")),
+      const SizedBox(width: 16),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text('Title',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                  color: Colors.black87)),
+          const SizedBox(height: 8),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Xuất bản:'),
+              Text('publishedDate'),
+            ],
+          ),
+          const SizedBox(height: 8),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Thể loại:'),
+              Text('category'),
+            ],
+          ),
+        ],
+      ),
+    ]),
+  );
 }
